9c4e2992791824186928ae7355db54f3a7fc611e
Kalpana <kalpanayadav21072006@gmail.com>
2026-01-03 20:32:13 +0530

    final change done

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,36 +1,4 @@
 
-// import { NextResponse } from "next/server";
-// import connectDB from "@/lib/mongodb";
-// import Product from "@/models/Product";
-// import mongoose from "mongoose";
-
-// type RouteContext = {
-//   params: {
-//     id: string;
-//   };
-// };
-// export async function GET(
-//   req: Request,
-//   { params }: RouteContext
-// ) {
-//   try {
-//     await connectDB();
-
-//     if (!mongoose.Types.ObjectId.isValid(params.id)) {
-//       return NextResponse.json(null, { status: 404 });
-//     }
-
-//     const product = await Product.findById(params.id);
-
-//     if (!product) {
-//       return NextResponse.json(null, { status: 404 });
-//     }
-
-//     return NextResponse.json(product);
-//   } catch (err) {
-//     return NextResponse.json(null, { status: 500 });
-//   }
-// }
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Product from "@/models/Product";
